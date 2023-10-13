99ffe26d435ada8a8fa58ed4c3ca81af10030975
Dima163163 <khlopovdima@gmail.com>
2023-10-13 13:53:29 +0400

    BOM2 Lesson1

---
+++ script.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const createInput = () => {
+  const input = document.createElement('input');
+  document.querySelector('body').append(input);
+};
+
+const createParagraph = () => {
+  const p = document.createElement('p');
+  document.querySelector('body').append(p)
+};
+
+const showText = () => {
+  document.querySelector('input').addEventListener('blur', () => {
+    setTimeout(() => {
+      document.querySelector('p').textContent =
+      document.querySelector('input').value;
+    }, 300);
+  });
+};
+
+const init = () => {
+  createInput();
+  createParagraph();
+  showText();
+};
+
+init();
